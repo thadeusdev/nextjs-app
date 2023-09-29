7a12980597065848335809b211763d82f6af474f
thadeus <thadeusnyariki94@gmail.com>
2023-09-29 16:23:04 +0300

    added components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>&#64;2023 Damon. All rights reserved.</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer
