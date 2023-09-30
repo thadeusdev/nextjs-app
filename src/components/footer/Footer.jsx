12e2e19ab4cbede709a7b051b6df6177af046f6b
thadeus <thadeusnyariki94@gmail.com>
2023-09-30 14:15:10 +0300

    styled footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './footer.module.css'
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>&#64;2023 Damon. All rights reserved.</div>
       <div></div>
     </div>
