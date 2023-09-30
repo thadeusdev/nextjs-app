a88b976549f879e568fc5ef7c88d88c492129908
thadeus <thadeusnyariki94@gmail.com>
2023-09-30 14:31:55 +0300

    added socials in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image';
+
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>&#64;2023 Damon. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="damon Facebook account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="damon Facebook account"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="damon Facebook account"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="damon Facebook account"
+        />
+      </div>
     </div>
   );
 }
