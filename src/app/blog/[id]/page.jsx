5b99d0c58c35760c5535542c2cd77e75458e3cd5
thadeus <thadeusnyariki94@gmail.com>
2023-10-08 20:07:59 +0300

    fetch data on client side

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,20 +1,31 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from './page.module.css'
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({params}) => {
+
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Reprehenderit, neque.
+            {data.title}
           </h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
-            earum minima vitae id natus cupiditate aliquam dolore eaque rem
-            culpa!
+            {data.body}
           </p>
           <div className={styles.author}>
             <Image
@@ -38,9 +49,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          quod, inventore accusamus voluptatum deleniti ducimus veritatis! Eum
-          odio quaerat dicta.
+          {data.body}
         </p>
       </div>
     </div>
