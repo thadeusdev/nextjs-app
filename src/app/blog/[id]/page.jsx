6f55c6a36ef8d0b45a18563254656f1bfdcb66f6
thadeus <thadeusnyariki94@gmail.com>
2023-10-09 21:46:20 +0300

    Next.js API Folder and CRUD Operations (App Router)

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,12 +1,13 @@
-import Image from 'next/image';
 import React from 'react';
-import styles from './page.module.css'
+import styles from './page.module.css';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: 'no-store',
   });
+
   if (!res.ok) {
     return notFound();
   }
@@ -14,43 +15,39 @@ async function getData(id) {
   return res.json();
 }
 
-const BlogPost = async ({params}) => {
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
 
+const BlogPost = async ({ params }) => {
   const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>
-            {data.title}
-          </h1>
-          <p className={styles.desc}>
-            {data.body}
-          </p>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>John Doe</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={data.img} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-          {data.body}
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
   );
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -7,6 +7,7 @@ async function getData() {
   const res = await fetch('http://localhost:3000/api/posts', {
     cache: 'no-store',
   });
+
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
@@ -15,15 +16,18 @@ async function getData() {
 }
 
 const Blog = async () => {
-
-  const data = await getData()
+  const data = await getData();
   return (
-    <div className={styles.mainCotainer}>
+    <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container} key={item.id}>
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
           <div className={styles.imageContainer}>
             <Image
-              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={item.img}
               alt=""
               width={400}
               height={250}
@@ -32,7 +36,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.body}</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
